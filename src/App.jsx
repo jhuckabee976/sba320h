b822f71837891ec4dfad82c1978429258770e901
jhuckabee976 <jhuckabee976@gmail.com>
2024-09-27 12:33:57 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <div className="main-content">
+      </div>
+    </div>
+  );
+}
+
+export default App;
